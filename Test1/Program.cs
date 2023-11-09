d04493263257c3549557631ace3a9626ea0d0211
hubert.haladyna <hubert.haladyna@BR-21>
2023-11-09 18:18:23 +0100

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+int i = 2;
+int b = 0;
+string a = Console.ReadLine();
+int n = int.Parse(a);
+
+while(b == 0)
+{
+    if(i >= n)
+    {
+        Console.WriteLine("TAK");
+        break;
+    }
+    else if(n % i == 0)
+    {
+        Console.WriteLine("NIE");
+        break;
+    }
+    else
+    {
+        i = i + 1;
+    }
+}
